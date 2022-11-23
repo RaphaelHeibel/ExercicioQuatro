f694fb5dcc0e160592f10e4e78883a62960227d1
RaphaelHeibel <raphaelrheibel@gmail.com>
2022-11-22 22:19:58 -0300

    Main [feature] bubble sort

---
+++ ExercicioQuatro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace ExercicioQuatro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = {5,3,2,4,7,1,0,6};
+
+            var sort = bubbleSort(v);
+
+            foreach (var item in sort)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int[] bubbleSort(int[] vetor)
+        {
+            int tamanho = vetor.Length;
+            int comparacoes = 0;
+            int trocas = 0;
+
+            for (int i = tamanho - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    comparacoes++;
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        int aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                        trocas++;
+                    }
+                }
+            }
+
+            return vetor;
+        }
+    }
+}
